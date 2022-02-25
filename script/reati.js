33fcb07451d038264d86bce233b827b121ee77d5
Bosie00 <100279498+Bosie00@users.noreply.github.com>
2022-02-25 22:27:55 +0100

    ho sonno

--- script/reati.js
+++ script/reati.js
@@ -13,3 +13,19 @@ const reati = [
         testo:"Ex do ullamco esse magna in amet excepteur nisi id occaecat qui dolor. Proident adipisicing velit amet ex ut nulla qui occaecat est excepteur deserunt ex dolor. Laboris aliqua sit aliquip nostrud ex adipisicing sit adipisicing Lorem id nostrud. Est reprehenderit veniam adipisicing labore ex aliqua et elit veniam commodo ex aliquip est."
     }
 ]
+
+
+window.onload = function() {
+    let container = document.querySelector("#container");
+    for (let i=0; i<reati.length; i++) {
+        let dets = document.createElement("details");
+        dets.className = "card";
+        let summ = document.createElement("summary");
+        summ.innerText = reati[i].reato;
+        let text = document.createElement("p");
+        text.innerText = reati[i].testo;
+        dets.appendChild(summ);
+        dets.appendChild(text);
+        container.appendChild(dets);
+    }
+}
